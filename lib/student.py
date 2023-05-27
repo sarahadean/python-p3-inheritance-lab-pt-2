59203314b3fd2c7c5bc9cca2daf405d0c32172c8
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-05-26 19:16:21 -0500

    all tests passed

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,34 @@
 class Student:
-    pass
+    def __init__(self):
+        self
+    
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    def __init__(self):
+        self
+    
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+    
+    def raise_hand(self):
+        for s in range(0, 10):
+            super().raise_hand()
+        
+        
+            
+# george = Student()
+# # george.raise_hand()
+
+# jessica = ChattyStudent()
+
+# # jessica.hello()
+# #successfully printed
+
+# jessica.raise_hand()
+    
